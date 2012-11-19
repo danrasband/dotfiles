0460ea260dda23f191ec09d1e76ffb2ce39f4223
Dan Rasband <dan@agencyfusion.com>
2012-11-19 14:25:26 -0700

    Organize rails aliases and add a couple

--- aliases/rails.sh
+++ aliases/rails.sh
@@ -1,7 +1,16 @@
+# Bundle
 alias b="bundle"
 alias be="bundle exec"
+
+# Rails
 alias berc="bundle exec rails console"
 alias berg="bundle exec rails generate"
+
+# Rake
 alias bert="bundle exec rake -T"
+alias berd="bundle exec rake db:migrate"
+alias berdr="bundle exec rake db:rollback"
+
+# Testing
 alias beg="bundle exec guard"
 
