097fcde060659260fbab2b290c92b1366560b02c
Dan Rasband <danrasband@gmail.com>
2014-08-05 16:40:16 -0600

    Fix formatting

--- functions/git.sh
+++ functions/git.sh
@@ -1,15 +1,19 @@
-function popstash() {
-  local n=$1
+function popstash {
+  local n=$1;
+
   if [ -z "$n" ]; then
     n="0";
   fi
+
   git stash pop stash@{$n};
 }
 
 function applystash {
-  local n=$1
+  local n=$1;
+
   if [ -z "$n" ]; then
     n="0";
   fi
+
   git stash apply stash@{$n};
 }
