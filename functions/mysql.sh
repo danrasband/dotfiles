c3e44c422d92ad5627bce36c3ef378729108dd47
Dan Rasband <danrasband@gmail.com>
2015-09-09 10:24:29 -0600

    Fix importdb function

--- functions/mysql.sh
+++ functions/mysql.sh
@@ -1,6 +1,6 @@
 importdb() {
-  local $dbname=$1
-  local $file=$2
+  local dbname=$1
+  local file=$2
 
   mysql -u root <<< "DROP DATABASE $dbname; CREATE DATABASE $dbname;"
   mysql -u root $dbname < $file
