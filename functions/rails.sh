5dda8a34e9e4e6d7d841b16bec04874bf0d931d4
Dan Rasband <danrasband@gmail.com>
2016-03-30 17:13:46 -0600

    Add output to dbreset

--- functions/rails.sh
+++ functions/rails.sh
@@ -10,7 +10,12 @@ function dbreset {
   fi
 
   export RAILS_ENV=$environment;
-  bundle exec rake db:drop && bundle exec rake db:create && bundle exec rake db:migrate;
+  echo "bundle exec rake db:drop" && \
+  bundle exec rake db:drop && \
+  echo "bundle exec rake db:create" && \
+  bundle exec rake db:create && \
+  echo "bundle exec rake db:migrate" && \
+  bundle exec rake db:migrate;
   if [ $? -eq 0 ]; then
     if [ "$environment" != "test" ]; then
       bundle exec rake db:seed;
