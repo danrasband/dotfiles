28bddcbbe2f06b984ba36199b4669b90e6d0b3ef
Dan Rasband <danrasband@gmail.com>
2017-12-16 22:07:44 -0700

    Add OTP function

---
+++ functions/otp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Generate one time passwords based on a QR code
+
+otp() {
+  local qrcode_path=$1;
+  local output=`zbarimg $qrcode_path`;
+  local secret=`sed -E "s/.*secret=([^&]+).*/\\1/" <<< $output`
+  local password=`oathtool --totp -b $secret`;
+  echo -n $password | pbcopy
+  echo "One time password copied to clipboard."
+}
