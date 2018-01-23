0f440ae2fe6a35a5abba633a9eda0d93dae0b6f0
Dan Rasband <danrasband@gmail.com>
2018-01-23 16:06:41 -0700

    Add some more docker-compose aliases

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,4 +1,6 @@
 # Docker Compose
 alias dcup='docker-compose up'
+alias dcstop='docker-compose stop'
+alias dc='docker-compose'
 alias dcr='docker-compose run'
 alias dcdrush='docker-compose -f docker-compose.drush.yml run --rm drush drush'
