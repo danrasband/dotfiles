eb808a98a15ea4a5e46331c31e48153e0b62d9ec
Daniel Rasband <daniel.rasband@aemcorp.com>
2021-01-12 17:11:55 -0700

    Remove unused git flow aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -13,16 +13,3 @@ alias gbD="git branch -D"
 alias grpo="git remote prune origin"
 alias grc="git rebase --continue"
 alias gslist="git stash list"
-
-# Git flow stuff.
-if [ -n `which git-flow` ]; then
-  alias gffs="git flow feature start"
-  alias gfff="git flow feature finish"
-  alias gfrs="git flow release start"
-  alias gfrf="git flow release finish"
-  alias gfhs="git flow hotfix start"
-  alias gfhf="git flow hotfix finish"
-  alias gfss="git flow support start"
-  alias gfsf="git flow support finish"
-fi
-
