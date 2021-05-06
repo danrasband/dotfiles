16f21f9983131f1a0bcc616c0f6ca24093a3ca24
Daniel Rasband <daniel.rasband@aemcorp.com>
2021-05-06 09:10:33 -0600

    Use Docker CLI for docker compose

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,9 +1,9 @@
 # Docker Compose
-alias dcup='docker-compose up'
-alias dcstop='docker-compose stop'
-alias dc='docker-compose'
-alias dcr='docker-compose run'
-alias dcdrush='docker-compose -f docker-compose.drush.yml run --rm drush drush'
+alias dcup='docker compose up'
+alias dcstop='docker compose stop'
+alias dc='docker compose'
+alias dcr='docker compose run'
+alias dcdrush='docker compose -f docker-compose.drush.yml run --rm drush drush'
 
 # Docker
 alias dpa='docker ps -a'
